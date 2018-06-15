d7f1d7827d077065904628ab0370998fd4b402d3
sydneypalumbo <sydney.palumbo@smartcar.com>
2018-06-15 11:04:12 -0700

    style changes and request ui ux

--- src/routes/vehicle-view.js
+++ src/routes/vehicle-view.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import './scss/login.scss'
+import './scss/vehicle-view.scss'
 import axios from 'axios'
 
-const STATIC_TEXT = [
-    {type: 'LOCATE', message: 'REFRESH LOCATION'},
-    {type: 'LOCK', message: 'LOCK THIS CAR'},
-    {type: 'UNLOCK', message: 'UNLOCK THIS CAR'},
-    {type: 'ODOMETER', message: 'CHECK ODOMETER'},
+const BUTTONS = [
+    {type: 'LOCATE', text: 'REFRESH LOCATION', message: 'Want to check on the location of your child`s car?'},
+    {type: 'LOCK', text: 'LOCK THIS CAR', message: 'Need to make sure the doors were locked?'},
+    {type: 'UNLOCK', text: 'UNLOCK THIS CAR', message: 'Someone accidentally get locked out?'},
+    {type: 'ODOMETER', text: 'CHECK ODOMETER', message: 'Time to get the car serviced?'},
 ]
+
+const RESPONSES = {
+    LOCATE: 'Successfully located this vehicle - check out the map!',
+    LOCK: 'Successfully locked this vehicle!',
+    UNLOCK: 'Successfully unlocked this vehicle!',
+    ODOMETER: 'The odometer reading is '
+}
 
 export default class VehicleView extends React.Component{
 
@@ -32,28 +39,52 @@ export default class VehicleView extends React.Component{
     }
 
     makeVehicleRequest(type) {
-        console.log('made call')
+        this.setState({
+            message: null
+        })
         const vehicle = this.props.location.state.vehicle
         axios.get('/request', {params: {
             type: type,
             id: vehicle.id
         }})
             .then((res)=>{
-                if (type === 'LOCATE') {
-                    this.setState({
-                        location: res.data.data
-                    })
+                switch(type) {
+                    case 'LOCATE':
+                        this.setState({
+                            location: res.data.data,
+                            message: RESPONSES.LOCATE
+                        })
+                        break
+                    case 'LOCK':
+                        this.setState({
+                            message: RESPONSES.LOCK
+                        })
+                        break
+                    case 'UNLOCK':
+                        this.setState({
+                            message: RESPONSES.UNLOCK
+                        })
+                        break
+                    default:
+                        this.setState({
+                            message: RESPONSES.ODOMETER + `${res.data.data.distance} km!`
+                        })
+
                 }
+                console.log(res)
             })
     }
 
     buildButtons() {
-        return STATIC_TEXT.map((action) => {
+        return BUTTONS.map((action) => {
             return(
-                <button className="button request-button"
-                        onClick={() => {this.makeVehicleRequest(action.type)}}>
-                    {action.message}
-                </button>
+                <div>
+                    <div className="request-button-prompt">{action.message}</div>
+                    <button className="button request-button"
+                            onClick={() => {this.makeVehicleRequest(action.type)}}>
+                        {action.text}
+                    </button>
+                </div>
             )
         })
     }
@@ -62,18 +93,26 @@ export default class VehicleView extends React.Component{
     render(){
         const vehicle = this.props.location.state.vehicle
         return(
-            <div>
-                <img className="background"/>
-                {this.state.location &&
-                        <iframe
-                            width="600"
-                            height="600"
-                            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBjJTE9t8cl5g_2Gzh94q47X1AuyvXh6Gc
-                            &q=${this.state.location.latitude},${this.state.location.longitude}`}
-                        />
+            <div className="vehicle-view-wrapper">
+                <img src="https://sbi.insure/wp-content/uploads/2017/10/Teenager-Driving.jpg" className="background"/>
+                {this.state.message && <div className="vehicle-view-message">{this.state.message}</div>}
+                <div className='vehicle-view-left'>
+                    {this.state.location &&
+                            <iframe
+                                className='google-maps'
+                                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBjJTE9t8cl5g_2Gzh94q47X1AuyvXh6Gc
+                                &q=${this.state.location.latitude},${this.state.location.longitude}`}
+                            />
                     }
-                    <div>{vehicle.year} {vehicle.make} {vehicle.model}</div>
-                {this.buildButtons()}
+                </div>
+                <div className='vehicle-view-right'>
+                    <div className="vehicle-view-header">
+                        {vehicle.year} {vehicle.make} {vehicle.model}
+                    </div>
+                    <div className="request-button-wrapper">
+                        {this.buildButtons()}
+                    </div>
+                </div>
             </div>
         )
     }
